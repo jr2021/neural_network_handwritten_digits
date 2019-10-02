d8c8d59315d818dcf1015b8cd83a9e9b2700fec7
Jake Robertson <jake.robertson@queensu.ca>
2019-10-02 14:20:47 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,8 +5,5 @@ def main():
     perceptron.train('7', '5', 100)
     print(perceptron.test('7', '5'))
 
-    # nearest_neighbor = NearestNeighbor("mnist-x.data", "mnist-y.data")
-    # print(nearest_neighbor.test())
-
 if __name__ == '__main__':
     main()
